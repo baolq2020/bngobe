6c702b947e9f909752556ead3ff9ad4268a56226
bảo lý <baolq1996@gmail.com>
2021-02-05 16:08:52 +0700

    Init commit

--- controllers/DetectController.go
+++ controllers/DetectController.go
@@ -12,11 +12,22 @@ func DetectImage(c *gin.Context) {
 
 	file, _ , _ := c.Request.FormFile("file")
 
-    content, _ := ioutil.ReadAll(file)
+	content, _ := ioutil.ReadAll(file)
 
-    // Encode as base64.
+	// Encode as base64.
 	encoded := base64.StdEncoding.EncodeToString(content)
 	
+	mimeType := http.DetectContentType(content)
+
+	switch mimeType {
+		case "image/png":
+			encoded = "data:image/png;base64," + encoded
+		case "image/jpeg":
+			encoded = "data:image/jpeg;base64," + encoded
+		default:
+			encoded = encoded
+	}
+
 	services.SendImagesToKafka(encoded)
 
 	c.JSON(http.StatusOK, gin.H{"data": "success"})
